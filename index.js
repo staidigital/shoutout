174b65ebcc7bddc21fe74e45359b91ea900d073e
Sondre Stai <sondre.stai@gmail.com>
2017-02-08 14:45:44 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var app = require('express')(); //webserveren
+var http = require('http').Server(app);
+var webSocket = require('socket.io')(http);
+var _ = require('lodash');
+
+var questions = [];
+var id = 0;
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+http.listen(3000,function(){
+  console.log('Listening on 3000');
+});
+
+//nå klient kobler seg på
+webSocket.on('connection',function(socket){
+  console.log('new connection');
+  //laste inn tidligere stilte spørsmål
+  socket.emit('all questions', JSON.stringify(questions));
+
+  //når det kommer et nytt spørsmål fra klient
+  socket.on('new question', function(question){
+    var q =
+    {
+      'text': question,
+      'id': id,
+      'votes': 0,
+      'answered': false
+    }
+
+    id++;
+    questions.push(q);
+    webSocket.emit('new question', JSON.stringify(q));
+  });
+
+  //ny stemme
+  socket.on('vote', function(vote){
+    console.log('vote received');
+    var vote = JSON.parse(vote);
+    var q = _.find(questions, function(q){
+      return q.id == vote.id;
+    });
+    if(q == null) {
+      console.log('question not found');
+      return;
+    }
+    if(vote.vote == 'plus') q.votes++;
+    else q.votes--;
+    console.log('sending vote');
+    webSocket.emit('vote', JSON.stringify(q));
+  });
+});
