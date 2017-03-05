e92d60ab5aedb89e5b888afdae2f750090f83c12
haakoaho <haakoaho@stud.ntnu.no>
2017-03-05 11:54:23 +0100

    Egen mappe for tester
    kjør med å skrive npm test tests/example.js

--- tests/example.js
+++ tests/example.js
@@ -1,13 +1,20 @@
 var expect = require("chai").expect;
-var shoutout=require("./index.js")
+var shoutout=require("../index.js")
 var request=require("request")
 
 describe("test1", function() {
     var url = "http://localhost:3001/"
+    var url2="http://localhost:3001/thispagedoesnotexist.html"
     it("åpner nettsiden", function(done) {
       request(url, function(error, response, body) {
       expect(response.statusCode).to.equal(200);
       done();
+    });
+  });
+  it("åpner en side som ikke finnes",function(done){
+    request(url2,function(error,response,body){
+    expect(response.statusCode).to.equal(404);
+    done();
     });
   });
 })
