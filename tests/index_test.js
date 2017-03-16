30340a5c548eb508da2d1e238721f8f8adf0a95a
haakoaho <haakoaho@stud.ntnu.no>
2017-03-16 19:29:01 +0100

    mer tester

--- tests/index_test.js
+++ tests/index_test.js
@@ -2,7 +2,7 @@ var expect = require("chai").expect;
 var request=require("request");
 var io     = require('socket.io-client');
 var SocketTester = require('socket-tester');
-var socket
+var client1,client2
 var app = require('../index');
 var socketUrl = 'http://localhost:3001';
 
@@ -49,20 +49,35 @@ describe("http", function() {
   });
 })
 
+describe("rooms",function(){
+    it("lager et rom",function(done){
+    client1={
+      on:{
+        'created room':socketTester.shouldBeCalledNTimes(1)
+      },
+      emit:{
+        'create room':'tdt4145'
+      }
+    }
+    socketTester.run([client1],done);
+    });
+   });
+
  describe("spørsmål",function(){
      it("sender spørsmål til et eksisterende rom",function(done){
-       var client1={
+    client1={
          on:{
            'new question':function(data){
           text=JSON.parse(data).text;
           expect(text).to.equal("Why should we use SCRUM?")
-           }
+        },
+           'new question':socketTester.shouldBeCalledNTimes(1),
          },
        emit: {
          'create room':'tdt4145',
        }
      };
-     var client2={
+ client2={
        emit:{
        'join room' :'tdt4145',
        'new question':'Why should we use SCRUM?'
@@ -71,4 +86,23 @@ describe("http", function() {
 
     socketTester.run([client1,client2],done);
      });
+
+   it("sender spørsmål til et ikke-eksisterende rom",function(done){
+     client1={
+          on:{
+            'new question':socketTester.shouldNotBeCalled()
+          },
+        emit: {
+          'create room':'tdt4145',
+        }
+      };
+      client2={
+            emit:{
+            'join room' :'tdt4105',
+            'new question':'Why should we use SCRU'
+          }
+        };
+    socketTester.run([client1,client2],done);
+   });
+
  })
