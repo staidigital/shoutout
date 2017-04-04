9f570fb3733ab41a465340244d7ea4760f0ed1de
haakoaho <haakoaho@stud.ntnu.no>
2017-04-04 10:24:51 +0200

    Revert "Starten på loginsession testing"
    This reverts commit dccfcbd9eaddaa5a5ad89bb5d55772153ca2bd7d.

--- test.js
+++ test.js
@@ -5,7 +5,6 @@ var app=require('./index')
 var SocketTester = require('socket-tester');
 var client1,client2,client3
 var socketUrl = 'http://localhost:3001';
-var superagent=require('superagent');
 
 
 
@@ -49,17 +48,6 @@ describe("http", function() {
     });
   });
 })
-
-describe("innloging",function(){
-     it("logger inn på admin",function(done){
-     superagent('http://localhost:3001/login.html')
-     .send({username: "admin", password: "admin"})
-     .end(function(err,res){
-       console.log(res.statusCode);
-       done();
-     });
-     });
-});
 
  describe("spørsmål",function(){
      it("sender spørsmål til et eksisterende rom",function(done){
